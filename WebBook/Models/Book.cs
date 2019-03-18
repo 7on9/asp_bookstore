24c4350b19537b23fc2ada9fd0c31843e16fb21f
Administrator <Administrator@PC024>
2019-03-18 10:28:51 +0700

    new file

---
+++ WebBook/Models/Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBook.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+
+        public Book(Book a)
+        {
+            this.author = a.author;
+            this.id = a.id;
+            this.image_cover = a.image_cover;
+            this.title = a.title;
+        }
+
+        public Book()
+        {
+
+        }
+
+        public Book (int id, string title, string author, string image_cover)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = Id;}
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = Title; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = Author; }
+        }
+
+        public string ImageCover
+        {
+            get { return image_cover; }
+            set { image_cover = ImageCover; }
+        }
+    }
+}
